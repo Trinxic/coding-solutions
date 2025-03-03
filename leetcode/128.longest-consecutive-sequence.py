6893c994aff7c8b96f998de974fa736f85add235
Trinxic <davida_2016@outlook.com>
2025-03-02 20:44:57 -0500

    Added solution for LeetCode problem 128.

--- leetcode/128.longest-consecutive-sequence.py
+++ leetcode/128.longest-consecutive-sequence.py
@@ -1,26 +1,32 @@
 # @leet start
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        sequences: dict[int:int] = {}
+        if not nums:
+            return 0
+
+        sequences: dict[int: list[int]] = {}
+        longest: int = 0
+        seq_min: int = 0
+        seq_max: int = 1
         for i, num in enumerate(nums):
             if num in sequences:
                 continue  # ignore duplicates
-            sequences[num] = num
+
+            lower = upper = num
+            sequences[num] = [lower, upper]  # num<-num->num
+
             if num - 1 in sequences:
-                if num > sequences[sequences[num - 1]]:
-                    sequences[sequences[num - 1]] = num
-                    sequences[num] = sequences[num - 1]
+                lower = sequences[num - 1][seq_min]
+                upper = max(sequences[sequences[num - 1][seq_min]][seq_max], upper)
+
             if num + 1 in sequences:
-                val = sequences[num]
-                if val < sequences[sequences[num + 1]]:
-                    sequences[sequences[num + 1]] = val
-                    sequences[val] = sequences[num + 1]
-        return sequences
+                lower = min(sequences[sequences[num + 1][seq_max]][seq_min], lower)
+                upper = sequences[num + 1][seq_max]
 
+            # ...<-lower->upper  and  lower<-upper->...
+            sequences[lower][seq_max], sequences[upper][seq_min] = upper, lower
+            sequences[num] = [lower, upper]  # lower<-num->upper
+            longest = max(longest, upper - lower + 1)
 
+        return longest
 # @leet end
-
-"""
-
-"""
-
