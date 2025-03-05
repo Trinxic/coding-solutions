363a9672ea99ba831ff6030ef9642001a65f1cc8
Trinxic <davida_2016@outlook.com>
2025-03-05 18:51:50 -0500

    Revised solution for leetcode 128. New solution for 238.

--- leetcode/128.longest-consecutive-sequence.py
+++ leetcode/128.longest-consecutive-sequence.py
@@ -1,32 +1,13 @@
 # @leet start
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
-        sequences: dict[int: list[int]] = {}
+        nums_set: set[int] = set(nums)
         longest: int = 0
-        seq_min: int = 0
-        seq_max: int = 1
-        for i, num in enumerate(nums):
-            if num in sequences:
-                continue  # ignore duplicates
-
-            lower = upper = num
-            sequences[num] = [lower, upper]  # num<-num->num
-
-            if num - 1 in sequences:
-                lower = sequences[num - 1][seq_min]
-                upper = max(sequences[sequences[num - 1][seq_min]][seq_max], upper)
-
-            if num + 1 in sequences:
-                lower = min(sequences[sequences[num + 1][seq_max]][seq_min], lower)
-                upper = sequences[num + 1][seq_max]
-
-            # ...<-lower->upper  and  lower<-upper->...
-            sequences[lower][seq_max], sequences[upper][seq_min] = upper, lower
-            sequences[num] = [lower, upper]  # lower<-num->upper
-            longest = max(longest, upper - lower + 1)
-
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                i = 1
+                while num + i in nums_set:
+                    i += 1
+                longest = max(i, longest)
         return longest
 # @leet end
